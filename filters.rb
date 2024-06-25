92aa1674b2289d466e450c45c6a8cd045586324e
PeterDevelops <PeterDinhBC@gmail.com>
2024-06-24 17:21:33 -0700

    complete enumerable methods

--- filters.rb
+++ filters.rb
@@ -1,16 +1,33 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
 
-def find(id)
-    # Your code Here
+def find(candidates, id)
+  candidates.find { |candidate| candidate[:id] == id }
   end
   
   def experienced?(candidate)
-    # Your code Here
+    # checks if a single candidate has 2 or more years of experience
+    # returns true or false
+    candidate[:years_of_experience] >= 2
   end
+
   
   def qualified_candidates(candidates)
-    # Your code Here
+    
+    candidates.select do |candidate|
+      candidate[:years_of_experience] >= 2 &&
+      candidate[:github_points] >= 100 &&
+      know_ruby_or_python(candidate) &&
+      candidate[:date_applied] >= 15.days.ago.to_date &&
+      candidate[:age] > 17
+    end
   end
   
+  def ordered_by_qualification(candidates)
+    candidates.sort_by { |candidate| [-candidate[:years_of_experience], -candidate[:github_points]] }
+  end
+
   # More methods will go below
+  def know_ruby_or_python(candidate)
+    candidate[:languages].include?("Ruby") || candidate[:languages].include?("Python")
+  end
--- main.rb
+++ main.rb
@@ -6,4 +6,24 @@ require './filters'
 
 ## Your test code can go here
 
-pp @candidates
+# pp @candidates
+# candidate =  {
+#   id: 5,
+#   years_of_experience: 4,
+#   github_points: 293,
+#   languages: ['C', 'Ruby', 'Python', 'Clojure'],
+#   date_applied: 5.days.ago.to_date,
+#   age: 26
+# }
+# pp experienced?(candidate)
+
+# candidate = find(@candidates, 10)
+# if candidate
+#   pp "Found candidate #{candidate[:id]}"
+# else
+#   pp "Could not find candidate"
+# end
+
+# pp qualified_candidates(@candidates)
+# create arrays
+pp ordered_by_qualification(@candidates)
